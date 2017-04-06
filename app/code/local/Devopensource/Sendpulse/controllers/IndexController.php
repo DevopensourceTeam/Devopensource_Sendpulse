01096b3891285037fdbc12361e38504d37ced164
Nacho Valera <nacho.valera@devopensource.com>
2017-04-06 12:52:50 +0200

    removed debug

--- app/code/local/Devopensource/Sendpulse/controllers/IndexController.php
+++ app/code/local/Devopensource/Sendpulse/controllers/IndexController.php
@@ -1,8 +1,5 @@
 <?php
 
 class Devopensource_Sendpulse_IndexController extends Mage_Core_Controller_Front_Action{
-    public function indexAction(){
-        echo "debug something here";
-        Mage::log('hello: '. Mage::getStoreConfig('sendpulse/general/active', Mage::app()->getStore()));
-    }
+
 }
